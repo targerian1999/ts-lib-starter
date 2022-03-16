4dc3229bf490829ff13049aed65d34431a6a3378
Muluneh Derese <muluneh.derese@protonmail.com>
2022-03-16 23:06:13 +0300

    feat(*): configure eslint

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+/**
+ * @packageDocumentation A library starter that just print passed name
+ */
+
+/**
+ * Print the name that is passed with adding 'Hello ' string
+ *
+ * @param name - name to be returned
+ *
+ * @public
+ */
+export function hello(name: string): string {
+  // let a = 0;
+  return "Hello " + name;
+}
